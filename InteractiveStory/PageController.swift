631dccd35db1bb5ac46a24fa38cfeedeeddeee75
Devan Beitel <dbeitel@uabmc.edu>
2016-07-06 14:40:05 -0500

    Started interactive adventure with first segue

---
+++ InteractiveStory/PageController.swift
@@ -0,0 +1,48 @@
+//
+//  PageController.swift
+//  InteractiveStory
+//
+//  Created by Devan Beitel on 7/6/16.
+//  Copyright © 2016 Devan Beitel. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+    
+    var page: Page?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blueColor()
+        if let page = page {
+            print(page.story.text)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
