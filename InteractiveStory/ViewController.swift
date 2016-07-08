4a8fa6d0045c6a38f45f5ae96711d423526033ed
Devan Beitel <dbeitel@uabmc.edu>
2016-07-08 11:57:10 -0500

    iOS 10/Swift 3 -> iOS 9.3/Swift 2. Changed rawValues to custom rawValues for argument "name"

--- InteractiveStory/ViewController.swift
+++ InteractiveStory/ViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Error: ErrorType {
+        case NoName
+    }
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let story = Page(story: .TouchDown)
-        story.firstChoice = (title: "Some Title", page: Page(story: .Droid))
+        story.firstChoice = (title: "Some Title", page: Page(story: .Droid("Devan")))
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,10 +27,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
+    override func prepareForSegue (segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startAdventure" {
-            if let pageController = segue.destinationViewController as? PageController {
-                pageController.page = Adventure.adventure
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw Error.NoName
+                    }
+                    
+                    if let pageController = segue.destinationViewController as? PageController {
+                        pageController.page = Adventure.adventure("Devan")
+                    }
+                }
+            } catch {
+                
             }
         }
     }
